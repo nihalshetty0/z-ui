bde779f4f12343471b4a7eb59200d8451c47671f
Nihal Shetty <nihalnshetty9@gmail.com>
2024-10-01 13:45:07 +0530

    fix: :bug: better intergration of checkbox style with css variable to support theming

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -13,7 +13,14 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "group h-4 w-4 shrink-0 rounded-sm border border-icon-primary focus:outline-none focus:ring-2 focus:ring-focus focus:ring-offset-1 disabled:cursor-not-allowed disabled:border-icon-disabled aria-[invalid=true]:border-support-error data-[state=checked]:bg-icon-primary data-[state=indeterminate]:bg-icon-primary data-[state=checked]:text-white data-[state=indeterminate]:text-white",
+      [
+        "group h-4 w-4 shrink-0 rounded-sm border border-icon-primary bg-icon-primary text-icon-inverse data-[state=unchecked]:bg-inherit",
+        "focus:outline-none focus:ring-2 focus:ring-focus focus:ring-offset-1 focus:ring-offset-focus-inset",
+        "disabled:cursor-not-allowed disabled:border-icon-disabled",
+        "disabled:data-[state=checked]:border-transparent disabled:data-[state=checked]:bg-icon-disabled",
+        "disabled:data-[state=indeterminate]:border-transparent disabled:data-[state=indeterminate]:bg-icon-disabled",
+        "aria-[invalid=true]:border-support-error",
+      ],
       className,
     )}
     {...props}
